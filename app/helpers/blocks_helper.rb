dfc1907adc33f024b603fd70df77973de45ee8ac
Denis Ivanov <info@lakehouse.ru>
2018-06-18 19:04:30 +0300

    Add block title helper

--- app/helpers/blocks_helper.rb
+++ app/helpers/blocks_helper.rb
@@ -1,5 +1,9 @@
 module BlocksHelper
   def render_block (slug)
     Block.find_by(slug: slug).content.html_safe if slug.present? && Block.find_by(slug: slug).present?
+  end  
+
+  def render_block_title (slug)
+    Block.find_by(slug: slug).title if slug.present? && Block.find_by(slug: slug).present?
   end
 end
